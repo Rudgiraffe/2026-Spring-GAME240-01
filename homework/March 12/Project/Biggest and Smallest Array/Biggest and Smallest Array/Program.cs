17c26fea8c5ffa13065299828e8c67629349f502
Rudgiraffe <jessicatrudnick@gmail.com>
2026-05-05 14:48:40 -0700

    Add files via upload

---
+++ homework/March 12/Project/Biggest and Smallest Array/Biggest and Smallest Array/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] numbers = { 9, 50, 23, 99, -11};
+
+int SmallGuy = numbers [0];
+int LargeGuy = numbers [0];
+
+int Tim = 1;
+
+while (Tim < SmallGuy)
+{
+    if (numbers[Tim] > LargeGuy)
+        {
+        LargeGuy = numbers[Tim];
+        }
+    if (numbers[Tim] < SmallGuy)
+        {
+        SmallGuy = numbers[Tim];
+        }
+    Tim++;
+}
+
+Console.WriteLine("The largest number is: " + LargeGuy);
+Console.WriteLine("The smallest number is: " + SmallGuy);
