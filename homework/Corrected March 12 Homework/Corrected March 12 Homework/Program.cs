5967e1357c225ca5405aacdf1a91afe94b966f2d
Rudgiraffe <jessicatrudnick@gmail.com>
2026-05-26 09:52:02 +0100

    Add files via upload

---
+++ homework/Corrected March 12 Homework/Corrected March 12 Homework/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] numbers = { 9, 50, 23, 99, -11 };
+
+int SmallGuy = numbers[0];
+int LargeGuy = numbers[0];
+
+int Tim = 1;
+
+while (Tim < numbers.Length)
+{
+    if (numbers[Tim] > LargeGuy)
+    {
+        LargeGuy = numbers[Tim];
+    }
+
+    if (numbers[Tim] < SmallGuy)
+    {
+        SmallGuy = numbers[Tim];
+    }
+
+    Tim++;
+}
+
+Console.WriteLine("The largest number is: " + LargeGuy);
+Console.WriteLine("The smallest number is: " + SmallGuy);
